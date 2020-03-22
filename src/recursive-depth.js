33f69209e1ddc7d1d47f4d50096a1458da0ac9e9
Dmitry <59093285+Dmytri88@users.noreply.github.com>
2020-03-22 15:10:25 +0300

    added solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,16 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr) {
+        if (arr.findIndex(item => Array.isArray(item)) === -1){
+            return 1
+        } else {
+            var result = []
+            arr.forEach((el) => {
+                if (Array.isArray(el)) {
+                    var calculate = this.calculateDepth(el)
+                    result.push(calculate + 1)
+                }
+            });
+            return  result.sort((a,b) => {return b-a})[0]
+        }
     }
 };
